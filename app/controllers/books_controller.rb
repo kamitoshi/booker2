c3cfd63da33084ef24e18cd5ba3a92e1a090e2ff
kamitoshi <address>
2020-03-21 04:49:58 +0000

    [update] 各種ページの完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def edit
@@ -35,6 +36,12 @@ class BooksController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
